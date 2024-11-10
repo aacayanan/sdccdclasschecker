e43db3b38ae1ee987a44586af245834bfabd1abe
Osmium <aaroncayanan02@gmail.com>
2024-11-10 09:31:50 -0800

    Updated server.py to search classes based on class id

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-courses = [3398, 3400, 3127, 3121]
+courses = [81851, 82243, 81294, 81299]
 
 def get_data():
     url = 'https://mws-api.sdccd.edu/?term=2253career=ugrd&_=1730965648722'
@@ -20,20 +20,28 @@ def get_data():
         print(e)
 
 
+def get_row_number(data, class_num):
+    rows = data['data']['query']['rows']
+    for entry in rows:
+        if entry.get('CLASS_NBR') == class_num:
+            return entry.get('attr:rownumber')
+    return None
+
+
 def get_class_info(course_list):
     data = get_data()
     watchlist = []
     for course_num in course_list:
+        row = get_row_number(data, course_num) - 1  # -1 to account for 0-based index
         list_item = {
-            data['data']['query']['rows'][course_num]['COURSENAME']: {
-                'DAYS': data['data']['query']['rows'][course_num]['DAYS'],
-                'OPEN_SEATS': data['data']['query']['rows'][course_num]['TRUEOPEN'],
-                'WAITLIST': data['data']['query']['rows'][course_num]['WAIT_TOT']
+            data['data']['query']['rows'][row]['COURSENAME']: {
+                'DAYS': data['data']['query']['rows'][row]['DAYS'],
+                'OPEN_SEATS': data['data']['query']['rows'][row]['TRUEOPEN'],
+                'WAITLIST': data['data']['query']['rows'][row]['WAIT_TOT']
             }
         }
         watchlist.append(list_item)
     return watchlist
-
 
 @app.route("/")
 def retrieve_classes():
